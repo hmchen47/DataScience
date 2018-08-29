7014a9f01b7dd547d98c6792b5d2a892776f9ac7
H.-M. Fred Chen <h.m.chen@ieee.org>
2018-08-29 12:03:15 -0700

    feat: re-download utility python code & run Mod01 cells

--- AppliedDS-UMich/3-AML/notebooks/adspy_shared_utilities.py
+++ AppliedDS-UMich/3-AML/notebooks/adspy_shared_utilities.py
@@ -1,259 +1,263 @@
+# version 1.1
 
-<!DOCTYPE HTML>
-<html>
+import numpy
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.colors import ListedColormap, BoundaryNorm
+from sklearn import neighbors
+import matplotlib.patches as mpatches
+import graphviz
+from sklearn.tree import export_graphviz
+import matplotlib.patches as mpatches
 
-<head>
-    <meta charset="utf-8">
+def load_crime_dataset():
+    # Communities and Crime dataset for regression
+    # https://archive.ics.uci.edu/ml/datasets/Communities+and+Crime+Unnormalized
 
-    <title>adspy_shared_utilities.py (editing)</title>
-    <link rel="shortcut icon" type="image/x-icon" href="/user/elkljxyoytcwjbmkgctrtg/static/base/images/favicon.ico?v=30780f272ab4aac64aa073a841546240">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/static/components/jquery-ui/themes/smoothness/jquery-ui.min.css?v=9b2c8d3489227115310662a343fce11c" type="text/css" />
-    <link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/static/components/jquery-typeahead/dist/jquery.typeahead.min.css?v=7afb461de36accb1aa133a1710f5bc56" type="text/css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    crime = pd.read_table('CommViolPredUnnormalizedData.txt', sep=',', na_values='?')
+    # remove features with poor coverage or lower relevance, and keep ViolentCrimesPerPop target column
+    columns_to_keep = [5, 6] + list(range(11,26)) + list(range(32, 103)) + [145]  
+    crime = crime.ix[:,columns_to_keep].dropna()
+
+    X_crime = crime.ix[:,range(0,88)]
+    y_crime = crime['ViolentCrimesPerPop']
+
+    return (X_crime, y_crime)
+
+def plot_decision_tree(clf, feature_names, class_names):
+    # This function requires the pydotplus module and assumes it's been installed.
+    # In some cases (typically under Windows) even after running conda install, there is a problem where the
+    # pydotplus module is not found when running from within the notebook environment.  The following code
+    # may help to guarantee the module is installed in the current notebook environment directory.
+    #
+    # import sys; sys.executable
+    # !{sys.executable} -m pip install pydotplus
+
+    export_graphviz(clf, out_file="adspy_temp.dot", feature_names=feature_names, class_names=class_names, filled = True, impurity = False)
+    with open("adspy_temp.dot") as f:
+        dot_graph = f.read()
+    # Alternate method using pydotplus, if installed.
+    # graph = pydotplus.graphviz.graph_from_dot_data(dot_graph)
+    # return graph.create_png()
+    return graphviz.Source(dot_graph)
+
+def plot_feature_importances(clf, feature_names):
+    c_features = len(feature_names)
+    plt.barh(range(c_features), clf.feature_importances_)
+    plt.xlabel("Feature importance")
+    plt.ylabel("Feature name")
+    plt.yticks(numpy.arange(c_features), feature_names)
+
+def plot_labelled_scatter(X, y, class_labels):
+    num_labels = len(class_labels)
+
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    marker_array = ['o', '^', '*']
+    color_array = ['#FFFF00', '#00AAFF', '#000000', '#FF00AA']
+    cmap_bold = ListedColormap(color_array)
+    bnorm = BoundaryNorm(numpy.arange(0, num_labels + 1, 1), ncolors=num_labels)
+    plt.figure()
+
+    plt.scatter(X[:, 0], X[:, 1], s=65, c=y, cmap=cmap_bold, norm = bnorm, alpha = 0.40, edgecolor='black', lw = 1)
+
+    plt.xlim(x_min, x_max)
+    plt.ylim(y_min, y_max)
+
+    h = []
+    for c in range(0, num_labels):
+        h.append(mpatches.Patch(color=color_array[c], label=class_labels[c]))
+    plt.legend(handles=h)
+
+    plt.show()
+
+
+def plot_class_regions_for_classifier_subplot(clf, X, y, X_test, y_test, title, subplot, target_names = None, plot_decision_regions = True):
+
+    numClasses = numpy.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X[:, 0].min()
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    x2, y2 = numpy.meshgrid(numpy.arange(x_min-k, x_max+k, h), numpy.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(numpy.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+
+    if plot_decision_regions:
+        subplot.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    subplot.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    subplot.set_xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    subplot.set_ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        subplot.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    subplot.set_title(title)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        subplot.legend(loc=0, handles=legend_handles)
+
+
+def plot_class_regions_for_classifier(clf, X, y, X_test=None, y_test=None, title=None, target_names = None, plot_decision_regions = True):
+
+    numClasses = numpy.amax(y) + 1
+    color_list_light = ['#FFFFAA', '#EFEFEF', '#AAFFAA', '#AAAAFF']
+    color_list_bold = ['#EEEE00', '#000000', '#00CC00', '#0000CC']
+    cmap_light = ListedColormap(color_list_light[0:numClasses])
+    cmap_bold  = ListedColormap(color_list_bold[0:numClasses])
+
+    h = 0.03
+    k = 0.5
+    x_plot_adjust = 0.1
+    y_plot_adjust = 0.1
+    plot_symbol_size = 50
+
+    x_min = X[:, 0].min()
+    x_max = X[:, 0].max()
+    y_min = X[:, 1].min()
+    y_max = X[:, 1].max()
+    x2, y2 = numpy.meshgrid(numpy.arange(x_min-k, x_max+k, h), numpy.arange(y_min-k, y_max+k, h))
+
+    P = clf.predict(numpy.c_[x2.ravel(), y2.ravel()])
+    P = P.reshape(x2.shape)
+    plt.figure()
+    if plot_decision_regions:
+        plt.contourf(x2, y2, P, cmap=cmap_light, alpha = 0.8)
+
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap_bold, s=plot_symbol_size, edgecolor = 'black')
+    plt.xlim(x_min - x_plot_adjust, x_max + x_plot_adjust)
+    plt.ylim(y_min - y_plot_adjust, y_max + y_plot_adjust)
+
+    if (X_test is not None):
+        plt.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cmap_bold, s=plot_symbol_size, marker='^', edgecolor = 'black')
+        train_score = clf.score(X, y)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
+
+    if (target_names is not None):
+        legend_handles = []
+        for i in range(0, len(target_names)):
+            patch = mpatches.Patch(color=color_list_bold[i], label=target_names[i])
+            legend_handles.append(patch)
+        plt.legend(loc=0, handles=legend_handles)
+
+    if (title is not None):
+        plt.title(title)
+    plt.show()
     
+def plot_fruit_knn(X, y, n_neighbors, weights):
+    X_mat = X[['height', 'width']].as_matrix()
+    y_mat = y.as_matrix()
+
+    # Create color maps
+    cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF','#AFAFAF'])
+    cmap_bold  = ListedColormap(['#FF0000', '#00FF00', '#0000FF','#AFAFAF'])
+
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X_mat, y_mat)
+
+    # Plot the decision boundary by assigning a color in the color map
+    # to each mesh point.
     
-<link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/static/components/codemirror/lib/codemirror.css?v=2336fb49f85e9fa887ada9af35223dce">
-<link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/static/components/codemirror/addon/dialog/dialog.css?v=c89dce10b44d2882a024e7befc2b63f5">
-
-    <link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/static/style/style.min.css?v=f6c09475baf6beabd41f8fe518601204" type="text/css"/>
+    mesh_step_size = .01  # step size in the mesh
+    plot_symbol_size = 50
     
+    x_min, x_max = X_mat[:, 0].min() - 1, X_mat[:, 0].max() + 1
+    y_min, y_max = X_mat[:, 1].min() - 1, X_mat[:, 1].max() + 1
+    xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, mesh_step_size),
+                         numpy.arange(y_min, y_max, mesh_step_size))
+    Z = clf.predict(numpy.c_[xx.ravel(), yy.ravel()])
 
-    <link rel="stylesheet" href="/user/elkljxyoytcwjbmkgctrtg/custom/custom.css" type="text/css" />
-    <script src="/user/elkljxyoytcwjbmkgctrtg/static/components/es6-promise/promise.min.js?v=f004a16cb856e0ff11781d01ec5ca8fe" type="text/javascript" charset="utf-8"></script>
-    <script src="/user/elkljxyoytcwjbmkgctrtg/static/components/requirejs/require.js?v=6da8be361b9ee26c5e721e76c6d4afce" type="text/javascript" charset="utf-8"></script>
-    <script>
-      require.config({
-          
-          urlArgs: "v=20180829022517",
-          
-          baseUrl: '/user/elkljxyoytcwjbmkgctrtg/static/',
-          paths: {
-            'auth/js/main': 'auth/js/main.min',
-            custom : '/user/elkljxyoytcwjbmkgctrtg/custom',
-            nbextensions : '/user/elkljxyoytcwjbmkgctrtg/nbextensions',
-            kernelspecs : '/user/elkljxyoytcwjbmkgctrtg/kernelspecs',
-            underscore : 'components/underscore/underscore-min',
-            backbone : 'components/backbone/backbone-min',
-            jquery: 'components/jquery/jquery.min',
-            bootstrap: 'components/bootstrap/js/bootstrap.min',
-            bootstraptour: 'components/bootstrap-tour/build/js/bootstrap-tour.min',
-            'jquery-ui': 'components/jquery-ui/ui/minified/jquery-ui.min',
-            moment: 'components/moment/moment',
-            codemirror: 'components/codemirror',
-            termjs: 'components/term.js/src/term',
-            typeahead: 'components/jquery-typeahead/dist/jquery.typeahead'
-          },
-	  map: { // for backward compatibility
-	    "*": {
-		"jqueryui": "jquery-ui",
-	    }
-	  },
-          shim: {
-            typeahead: {
-              deps: ["jquery"],
-              exports: "typeahead"
-            },
-            underscore: {
-              exports: '_'
-            },
-            backbone: {
-              deps: ["underscore", "jquery"],
-              exports: "Backbone"
-            },
-            bootstrap: {
-              deps: ["jquery"],
-              exports: "bootstrap"
-            },
-            bootstraptour: {
-              deps: ["bootstrap"],
-              exports: "Tour"
-            },
-            "jquery-ui": {
-              deps: ["jquery"],
-              exports: "$"
-            }
-          },
-          waitSeconds: 30,
-      });
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
-      require.config({
-          map: {
-              '*':{
-                'contents': 'services/contents',
-              }
-          }
-      });
+    # Plot training points
+    plt.scatter(X_mat[:, 0], X_mat[:, 1], s=plot_symbol_size, c=y, cmap=cmap_bold, edgecolor = 'black')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
 
-      define("bootstrap", function () {
-          return window.$;
-      });
+    patch0 = mpatches.Patch(color='#FF0000', label='apple')
+    patch1 = mpatches.Patch(color='#00FF00', label='mandarin')
+    patch2 = mpatches.Patch(color='#0000FF', label='orange')
+    patch3 = mpatches.Patch(color='#AFAFAF', label='lemon')
+    plt.legend(handles=[patch0, patch1, patch2, patch3])
 
-      define("jquery", function () {
-          return window.$;
-      });
-
-      define("jqueryui", function () {
-          return window.$;
-      });
-
-      define("jquery-ui", function () {
-          return window.$;
-      });
-      // error-catching custom.js shim.
-      define("custom", function (require, exports, module) {
-          try {
-              var custom = require('custom/custom');
-              console.debug('loaded custom.js');
-              return custom;
-          } catch (e) {
-              console.error("error loading custom.js", e);
-              return {};
-          }
-      })
-    </script>
-
+        
+    plt.xlabel('height (cm)')
+    plt.ylabel('width (cm)')
     
+    plt.show()
+
+def plot_two_class_knn(X, y, n_neighbors, weights, X_test, y_test):
+    X_mat = X
+    y_mat = y
+
+    # Create color maps
+    cmap_light = ListedColormap(['#FFFFAA', '#AAFFAA', '#AAAAFF','#EFEFEF'])
+    cmap_bold  = ListedColormap(['#FFFF00', '#00FF00', '#0000FF','#000000'])
+
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+    clf.fit(X_mat, y_mat)
+
+    # Plot the decision boundary by assigning a color in the color map
+    # to each mesh point.
     
-
-</head>
-
-<body class="edit_app " 
-data-base-url="/user/elkljxyoytcwjbmkgctrtg/"
-data-file-path="adspy_shared_utilities.py"
-
->
-
-<noscript>
-    <div id='noscript'>
-      Jupyter Notebook requires JavaScript.<br>
-      Please enable it to proceed.
-  </div>
-</noscript>
-
-<div id="header">
-  <div id="header-container" class="container">
-  <div id="ipython_notebook" class="nav navbar-brand pull-left"><a href="/user/elkljxyoytcwjbmkgctrtg/tree" title='dashboard'>
-<img src='/hub/logo' alt='Jupyter Notebook'/>
-</a></div>
-
-  
-
-  
-  
-
-    <span id="login_widget">
-      
-        <button id="logout" class="btn btn-sm navbar-btn">Logout</button>
-      
-    </span>
-
-  
-
-  
-
-<a href='/hub/home'
- class='btn btn-default btn-sm navbar-btn pull-right'
- style='margin-right: 4px; margin-left: 2px;'
->
-Control Panel</a>
-
-
-  
-
-<span id="save_widget" class="pull-left save_widget">
-    <span class="filename"></span>
-    <span class="last_modified"></span>
-</span>
-
-
-  </div>
-  <div class="header-bar"></div>
-
-  
-
-<div id="menubar-container" class="container">
-  <div id="menubar">
-    <div id="menus" class="navbar navbar-default" role="navigation">
-      <div class="container-fluid">
-          <p  class="navbar-text indicator_area">
-          <span id="current-mode" >current mode</span>
-          </p>
-        <button type="button" class="btn btn-default navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <i class="fa fa-bars"></i>
-          <span class="navbar-text">Menu</span>
-        </button>
-        <ul class="nav navbar-nav navbar-right">
-          <li id="notification_area"></li>
-        </ul>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">File</a>
-              <ul id="file-menu" class="dropdown-menu">
-                <li id="new-file"><a href="#">New</a></li>
-                <li id="save-file"><a href="#">Save</a></li>
-                <li id="rename-file"><a href="#">Rename</a></li>
-                <li id="download-file"><a href="#">Download</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Edit</a>
-              <ul id="edit-menu" class="dropdown-menu">
-                <li id="menu-find"><a href="#">Find</a></li>
-                <li id="menu-replace"><a href="#">Find &amp; Replace</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Key Map</li>
-                <li id="menu-keymap-default"><a href="#">Default<i class="fa"></i></a></li>
-                <li id="menu-keymap-sublime"><a href="#">Sublime Text<i class="fa"></i></a></li>
-                <li id="menu-keymap-vim"><a href="#">Vim<i class="fa"></i></a></li>
-                <li id="menu-keymap-emacs"><a href="#">emacs<i class="fa"></i></a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">View</a>
-              <ul id="view-menu" class="dropdown-menu">
-              <li id="toggle_header" title="Show/Hide the logo and notebook title (above menu bar)">
-              <a href="#">Toggle Header</a></li>
-              <li id="menu-line-numbers"><a href="#">Toggle Line Numbers</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
-              <ul id="mode-menu" class="dropdown-menu">
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="lower-header-bar"></div>
-
-
-</div>
-
-<div id="site">
-
-
-<div id="texteditor-backdrop">
-<div id="texteditor-container" class="container"></div>
-</div>
-
-
-</div>
-
-
-
-
-
-
+    mesh_step_size = .01  # step size in the mesh
+    plot_symbol_size = 50
     
+    x_min, x_max = X_mat[:, 0].min() - 1, X_mat[:, 0].max() + 1
+    y_min, y_max = X_mat[:, 1].min() - 1, X_mat[:, 1].max() + 1
+    xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, mesh_step_size),
+                         numpy.arange(y_min, y_max, mesh_step_size))
+    Z = clf.predict(numpy.c_[xx.ravel(), yy.ravel()])
 
+    # Put the result into a color plot
+    Z = Z.reshape(xx.shape)
+    plt.figure()
+    plt.pcolormesh(xx, yy, Z, cmap=cmap_light)
 
+    # Plot training points
+    plt.scatter(X_mat[:, 0], X_mat[:, 1], s=plot_symbol_size, c=y, cmap=cmap_bold, edgecolor = 'black')
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
 
-    <script src="/user/elkljxyoytcwjbmkgctrtg/static/edit/js/main.min.js?v=cc7536522bdf28a7f0059136b696fd6e" type="text/javascript" charset="utf-8"></script>
+    title = "Neighbors = {}".format(n_neighbors)
+    if (X_test is not None):
+        train_score = clf.score(X_mat, y_mat)
+        test_score  = clf.score(X_test, y_test)
+        title = title + "\nTrain score = {:.2f}, Test score = {:.2f}".format(train_score, test_score)
 
+    patch0 = mpatches.Patch(color='#FFFF00', label='class 0')
+    patch1 = mpatches.Patch(color='#000000', label='class 1')
+    plt.legend(handles=[patch0, patch1])
 
+    plt.xlabel('Feature 0')
+    plt.ylabel('Feature 1')
+    plt.title(title)
 
-</body>
-
-</html>
+    plt.show()
