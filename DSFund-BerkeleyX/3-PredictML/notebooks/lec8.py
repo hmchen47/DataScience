5e6dc092440b8103d79ae2e2135c05687d0a9b11
H.-M. Fred Chen <h.m.chen@ieee.org>
2018-09-03 13:35:05 -0700

    feat: remove blank linkes

--- DSFund-BerkeleyX/3-PredictML/notebooks/lec8.py
+++ DSFund-BerkeleyX/3-PredictML/notebooks/lec8.py
@@ -1,9 +1,5 @@
 
 # coding: utf-8
-
-# In[ ]:
-
-
 from datascience import *
 import numpy as np
 
@@ -13,60 +9,24 @@ plots.style.use('fivethirtyeight')
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# ## Classification Examples: Medicine
-
-# **Please run all cells before this cell, including the import cell at the top of the notebook.**
-
-# In[ ]:
-
-
+# Classification Examples: Medicine
 ckd = Table.read_table('ckd.csv').relabeled('Blood Glucose Random', 'Glucose')
 ckd.show(3)
 
-
-# In[ ]:
-
-
 ckd.group('Class')
 
-
-# In[ ]:
-
-
 ckd.scatter('White Blood Cell Count', 'Glucose', colors='Class')
-
-
-# In[ ]:
-
 
 ckd.scatter('Hemoglobin', 'Glucose', colors='Class')
 
 
-# ## Classification Examples: Counterfeit Banknotes
-
-# **Please run all cells before this cell, including the previous example cells and the import cell at the top of the notebook.**
-
-# In[ ]:
-
-
+# Classification Examples: Counterfeit Banknotes
 banknotes = Table.read_table('banknote.csv')
 banknotes
 
-
-# In[ ]:
-
-
 banknotes.scatter('WaveletVar', 'WaveletCurt', colors='Class')
 
-
-# In[ ]:
-
-
 banknotes.scatter('WaveletSkew', 'Entropy', colors='Class')
-
-
-# In[ ]:
-
 
 fig = plots.figure(figsize=(8,8))
 ax = Axes3D(fig)
