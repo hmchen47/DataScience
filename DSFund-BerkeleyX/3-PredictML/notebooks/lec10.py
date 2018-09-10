65501204fbadc0a37d2609a42c564153c252414d
H.-M. Fred Chen <h.m.chen@ieee.org>
2018-09-10 10:15:28 -0700

    feat: init Lec10 python code

--- DSFund-BerkeleyX/3-PredictML/notebooks/lec10.py
+++ DSFund-BerkeleyX/3-PredictML/notebooks/lec10.py
@@ -1,9 +1,5 @@
 
 # coding: utf-8
-
-# In[ ]:
-
-
 # HIDDEN
 
 from datascience import *
@@ -20,24 +16,12 @@ import numpy as np
 # ## Data collection
 
 # First, we'll gather the data.  It turns out the data is publicly available on the Oakland public records site.  I downloaded it and combined it into a single CSV file by myself before lecture.
-
-# In[ ]:
-
-
 lprs = Table.read_table('https://inferentialthinking.com/data/all-lprs.csv.gz', compression='gzip', sep=',')
-
-
-# In[ ]:
-
 
 lprs
 
 
 # Let's start by renaming some columns, and then take a look at it.
-
-# In[ ]:
-
-
 lprs.relabel('red_VRM', 'Plate')
 lprs.relabel('red_Timestamp', 'Timestamp')
 lprs
@@ -50,25 +34,13 @@ lprs
 # ## Searching for Individuals
 
 # As a warmup, we'll take a look at ex-Mayor Jean Quan's car, and where it has been seen.  Her license plate number is 6FCH845.  (How did I learn that?  Turns out she was in the news for getting $1000 of parking tickets, and [the news article](http://www.sfgate.com/bayarea/matier-ross/article/Jean-Quan-Oakland-s-new-mayor-gets-car-booted-3164530.php) included a picture of her car, with the license plate visible.  You'd be amazed by what's out there on the Internet...)
-
-# In[ ]:
-
-
 lprs.where('Plate', '6FCH845')
 
 
 # OK, so her car shows up 6 times in this data set.  However, it's hard to make sense of those coordinates.  I don't know about you, but I can't read GPS so well.
 # 
 # So, let's work out a way to show where her car has been seen on a map.  We'll need to extract the latitude and longitude, as the data isn't quite in the format that the mapping software expects: the mapping software expects the latitude to be in one column and the longitude in another.  Let's write some Python code to do that, by splitting the Location string into two pieces: the stuff before the comma (the latitude) and the stuff after (the longitude).
-
-# In[ ]:
-
-
 '(37.79871, -122.276221)'.split(',')
-
-
-# In[ ]:
-
 
 def get_latitude(s):
     before, after = s.split(',')         # Break it into two parts
@@ -82,24 +54,12 @@ def get_longitude(s):
 
 
 # Let's test it to make sure it works correctly.
-
-# In[ ]:
-
-
 get_latitude('(37.797558, -122.26935)')
-
-
-# In[ ]:
-
 
 get_longitude('(37.797558, -122.26935)')
 
 
 # Good, now we're ready to add these as extra columns to the table.
-
-# In[ ]:
-
-
 lprs = lprs.with_columns(
     'Latitude',  lprs.apply(get_latitude, 'Location'),
     'Longitude', lprs.apply(get_longitude, 'Location')
@@ -108,10 +68,6 @@ lprs
 
 
 # And at last, we can draw a map with a marker everywhere that her car has been seen.
-
-# In[ ]:
-
-
 jean_quan = lprs.where('Plate', '6FCH845').select('Latitude', 'Longitude', 'Timestamp')
 Marker.map_table(jean_quan)
 
@@ -121,10 +77,6 @@ Marker.map_table(jean_quan)
 # ## Poking around
 
 # Let's try another.  And let's see if we can make the map a little more fancy.  It'd be nice to distinguish between license plate reads that are seen during the daytime (on a weekday), vs the evening (on a weekday), vs on a weekend.  So we'll color-code the markers.  To do this, we'll write some Python code to analyze the Timestamp and choose an appropriate color.
-
-# In[ ]:
-
-
 import datetime
 
 def get_color(ts):
@@ -140,10 +92,6 @@ lprs.append_column('Color', lprs.apply(get_color, 'Timestamp'))
 
 
 # Now we can check out another license plate, this time with our spiffy color-coding.  This one happens to be the car that the city issues to the Fire Chief.
-
-# In[ ]:
-
-
 t = lprs.where('Plate', '1328354').select('Latitude', 'Longitude', 'Timestamp', 'Color')
 Marker.map_table(t)
 
@@ -151,10 +99,6 @@ Marker.map_table(t)
 # Hmm.  We can see a blue cluster in downtown Oakland, where the Fire Chief's car was seen on weekdays during business hours.  I bet we've found her office.  In fact, if you happen to know downtown Oakland, those are mostly clustered right near City Hall.  Also, her car was seen twice in northern Oakland on weekday evenings.  One can only speculate what that indicates.  Maybe dinner with a friend?  Or running errands?  Off to the scene of a fire?  Who knows.  And then the car has been seen once more, late at night on a weekend, in a residential area in the hills.  Her home address, maybe?
 
 # Let's look at another. This time, we'll make a function to display the map.
-
-# In[ ]:
-
-
 def map_plate(plate):
     sightings = lprs.where('Plate', plate)
     t = sightings.select('Latitude', 'Longitude', 'Timestamp', 'Color')
@@ -166,10 +110,6 @@ map_plate('5AJG153')
 # What can we tell from this?  Looks to me like this person lives on International Blvd and 9th, roughly.  On weekdays they've seen in a variety of locations in west Oakland.  It's fun to imagine what this might indicate -- delivery person? taxi driver? someone running errands all over the place in west Oakland?
 # 
 # We can look at another:
-
-# In[ ]:
-
-
 map_plate('6UZA652')
 
 
