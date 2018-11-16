192ac6463f63c17a7e32f1e0df12ff6ed777d37a
H.-M. Fred Chen <h.m.chen@ieee.org>
2018-11-16 01:19:30 -0800

    feat: remove blanks lines for Python code in Working with Text

--- AppliedDS-UMich/4-TextMining/notebooks/01-Working+With+Text.py
+++ AppliedDS-UMich/4-TextMining/notebooks/01-Working+With+Text.py
@@ -9,107 +9,57 @@
 
 # # Working With Text
 
-# In[ ]:
-
 text1 = "Ethics are built right into the ideals and objectives of the United Nations "
 
 len(text1) # The length of text1
-
-
-# In[ ]:
 
 text2 = text1.split(' ') # Return a list of the words in text2, separating by ' '.
 
 len(text2)
 
-
-# In[ ]:
-
 text2
 
-
-# <br>
 # List comprehension allows us to find specific words:
-
-# In[ ]:
 
 [w for w in text2 if len(w) > 3] # Words that are greater than 3 letters long in text2
 
-
-# In[ ]:
-
 [w for w in text2 if w.istitle()] # Capitalized words in text2
-
-
-# In[ ]:
 
 [w for w in text2 if w.endswith('s')] # Words in text2 that end in 's'
 
-
-# <br>
 # We can find unique words using `set()`.
-
-# In[ ]:
 
 text3 = 'To be or not to be'
 text4 = text3.split(' ')
 
 len(text4)
 
-
-# In[ ]:
-
 len(set(text4))
-
-
-# In[ ]:
 
 set(text4)
 
-
-# In[ ]:
-
 len(set([w.lower() for w in text4])) # .lower converts the string to lowercase.
-
-
-# In[ ]:
 
 set([w.lower() for w in text4])
 
-
 # ### Processing free-text
-
-# In[ ]:
 
 text5 = '"Ethics are built right into the ideals and objectives of the United Nations" #UNSG @ NY Society for Ethical Culture bit.ly/2guVelr'
 text6 = text5.split(' ')
 
 text6
 
-
-# <br>
 # Finding hastags:
-
-# In[ ]:
 
 [w for w in text6 if w.startswith('#')]
 
-
-# <br>
 # Finding callouts:
 
-# In[ ]:
-
 [w for w in text6 if w.startswith('@')]
-
-
-# In[ ]:
 
 text7 = '@UN @UN_Women "Ethics are built right into the ideals and objectives of the United Nations" #UNSG @ NY Society for Ethical Culture bit.ly/2guVelr'
 text8 = text7.split(' ')
 
-
-# <br>
 # 
 # We can use regular expressions to help us with more complex parsing. 
 # 
@@ -119,8 +69,6 @@ text8 = text7.split(' ')
 # * lowercase letter (`'a-z'`) 
 # * number (`'0-9'`)
 # * or underscore (`'_'`)
-
-# In[ ]:
 
 import re # import re - a module that provides support for regular expressions
 
