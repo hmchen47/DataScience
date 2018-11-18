20e1b16741e9c9cfaa54aeab663da8e70445d5de
H.-M. Fred Chen <h.m.chen@ieee.org>
2018-11-18 11:31:42 -0800

    feat: remove blank lines for ReExp python code

--- AppliedDS-UMich/4-TextMining/notebooks/01-Regex+with+Pandas+and+Named+Groups.py
+++ AppliedDS-UMich/4-TextMining/notebooks/01-Regex+with+Pandas+and+Named+Groups.py
@@ -9,8 +9,6 @@
 
 # # Working with Text Data in pandas
 
-# In[ ]:
-
 import pandas as pd
 
 time_sentences = ["Monday: The doctor's appointment is at 2:45pm.", 
@@ -22,68 +20,35 @@ time_sentences = ["Monday: The doctor's appointment is at 2:45pm.",
 df = pd.DataFrame(time_sentences, columns=['text'])
 df
 
-
-# In[ ]:
-
 # find the number of characters for each string in df['text']
 df['text'].str.len()
-
-
-# In[ ]:
 
 # find the number of tokens for each string in df['text']
 df['text'].str.split().str.len()
 
-
-# In[ ]:
-
 # find which entries contain the word 'appointment'
 df['text'].str.contains('appointment')
-
-
-# In[ ]:
 
 # find how many times a digit occurs in each string
 df['text'].str.count(r'\d')
 
-
-# In[ ]:
-
 # find all occurances of the digits
 df['text'].str.findall(r'\d')
-
-
-# In[ ]:
 
 # group and find the hours and minutes
 df['text'].str.findall(r'(\d?\d):(\d\d)')
 
-
-# In[ ]:
-
 # replace weekdays with '???'
 df['text'].str.replace(r'\w+day\b', '???')
-
-
-# In[ ]:
 
 # replace weekdays with 3 letter abbrevations
 df['text'].str.replace(r'(\w+day\b)', lambda x: x.groups()[0][:3])
 
-
-# In[ ]:
-
 # create new columns from first match of extracted groups
 df['text'].str.extract(r'(\d?\d):(\d\d)')
 
-
-# In[ ]:
-
 # extract the entire time, the hours, the minutes, and the period
 df['text'].str.extractall(r'((\d?\d):(\d\d) ?([ap]m))')
-
-
-# In[ ]:
 
 # extract the entire time, the hours, the minutes, and the period with group names
 df['text'].str.extractall(r'(?P<time>(?P<hour>\d?\d):(?P<minute>\d\d) ?(?P<period>[ap]m))')
