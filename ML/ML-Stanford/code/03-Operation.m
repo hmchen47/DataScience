d1cbae2a5b4b737cde39c152e2470707922b4a5a
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-03-08 09:37:19 -0800

    feat: add code for Addition and Scalar Multiplication

---
+++ ML/ML-Stanford/code/03-Operation.m
@@ -0,0 +1,21 @@
+% Initialize matrix A and B
+A = [1, 2, 4; 5, 3, 2]
+B = [1, 3, 4; 1, 1, 1]
+
+% Initialize constant s
+s = 2
+
+% See how element-wise addition works
+add_AB = A + B 
+
+% See how element-wise subtraction works
+sub_AB = A - B
+
+% See how scalar multiplication works
+mult_As = A * s
+
+% Divide A by s
+div_As = A / s
+
+% What happens if we have a Matrix + scalar?
+add_As = A + s
