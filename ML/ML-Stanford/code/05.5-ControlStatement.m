e37cf16e5892d433376eb94999c62e245c71ec2e
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-03-15 21:01:00 -0700

    feat: sample code for Control Statements: for, while, if statement

---
+++ ML/ML-Stanford/code/05.5-ControlStatement.m
@@ -0,0 +1,28 @@
+v = zeros(10,1);
+for i=1:10, 
+    v(i) = 2^i;
+end;
+% Can also use "break" and "continue" inside for and while loops to control execution.
+
+i = 1;
+while i <= 5,
+  v(i) = 100; 
+  i = i+1;
+end
+
+i = 1;
+while true, 
+  v(i) = 999; 
+  i = i+1;
+  if i == 6,
+    break;
+  end;
+end
+
+if v(1)==1,
+  disp('The value is one!');
+elseif v(1)==2,
+  disp('The value is two!');
+else
+  disp('The value is not one or two!');
+end
