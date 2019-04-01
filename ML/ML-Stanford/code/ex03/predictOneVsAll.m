a48343c660a498fafee01825d42139a6fc53cfcc
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-04-01 13:20:36 -0700

    feat: add code for 1.4.1 One-vs-all Prediction

--- ML/ML-Stanford/code/ex03/predictOneVsAll.m
+++ ML/ML-Stanford/code/ex03/predictOneVsAll.m
@@ -28,12 +28,9 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+% 
 
-
-
-
-
+[maxVal, p] = max(X * all_theta', [], 2);
 
 
 % =========================================================================
