8c8228547dff0e42895cdfab6516d52efdb7efcf
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-04-10 01:35:03 -0700

    feat: add code for gradient & regularization in Ex04

--- ML/ML-Stanford/code/ex04/nnCostFunction.m
+++ ML/ML-Stanford/code/ex04/nnCostFunction.m
@@ -3,6 +3,7 @@ function [J grad] = nnCostFunction(nn_params, ...
                                    hidden_layer_size, ...
                                    num_labels, ...
                                    X, y, lambda)
+
 %NNCOSTFUNCTION Implements the neural network cost function for a two layer
 %neural network which performs classification
 %   [J grad] = NNCOSTFUNCTON(nn_params, hidden_layer_size, num_labels, ...
@@ -62,6 +63,7 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% nnCostFunction(nn_params, input_layer_size, hidden_layer_size, num_labels, X, y, lambda)
 % Part 1: Feedforward the neural network and return the cost in the
 %         variable J. Verify with ex4.m
 
@@ -69,27 +71,43 @@ Theta2_grad = zeros(size(Theta2));
 
 y_matrix = eye(num_labels)(y,:);
 
-X = [ones(m, 1) X];
+a1 = [ones(m, 1) X];
 
-a1 = sigmoid(X * Theta1');
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1) a2];
 
-a1 = [ones(size(a1, 1), 1) a1];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
 
-h_theta = sigmoid(a1 * Theta2');
+J = sum(sum((-1.0 / m) * (y_matrix .* log(a3) + ...
+    (1.0 - y_matrix) .* log(1.0 - a3))));
 
-J = sum(sum((-1.0 / m) * (y_matrix .* log(h_theta) + ...
-    (1.0 - y_matrix) .* log(1.0 - h_theta))));
+% Part 2: Implement the backpropagation algorithm to compute the gradients
+%         Theta1_grad and Theta2_grad. You should return the partial derivatives of
+%         the cost function with respect to Theta1 and Theta2 in Theta1_grad and
+%         Theta2_grad, respectively. After implementing Part 2, you can check
+%         that your implementation is correct by running checkNNGradients
+
+d3 = a3 - y_matrix;
+
+d2 = (d3 *  Theta2(:, 2:end)) .* sigmoidGradient(z2);
+
+Delta1 = d2' * a1;
+
+Delta2 = d3' * a2;
+
+Theta1_grad = Delta1 / m;
+Theta2_grad = Delta2 / m;
 
 
+% Part 3: Implement regularization with the cost function and gradients.
 
+J = J + lambda / 2 / m * (sum(sum(Theta1(:, 2:end).^2)) + ...
+    sum(sum(Theta2(:, 2:end).^2)));
 
-
-
-
-
-
-
-
+Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + (lambda/m) * Theta1(:, 2:end);
+Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + (lambda/m) * Theta2(:, 2:end);
 
 
 % -------------------------------------------------------------
