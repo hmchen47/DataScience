1ef8227e1564692ad5f64bbc5e84f9b22c7658b5
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-04-12 15:52:00 -0700

    feat: add code for learning curve function of EX05

--- ML/ML-Stanford/code/ex05/learningCurve.m
+++ ML/ML-Stanford/code/ex05/learningCurve.m
@@ -53,10 +53,13 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i=1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i, :), lambda);
 
-
-
-
+    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta, lambda);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, lambda);
+ 
+end    
 
 
 % -------------------------------------------------------------
