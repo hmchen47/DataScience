aeacf4b985b19d0d8b3f723103b7a3328ec7e66d
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-04-17 15:13:56 -0700

    feat: add code for training & predict Dataset 3 of Ex06

--- ML/ML-Stanford/code/ex06/dataset3Params.m
+++ ML/ML-Stanford/code/ex06/dataset3Params.m
@@ -8,8 +8,12 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+% C = 1;
+C_set = [0.01 0.03 0.1 0.3 1 3 10 30];
+% sigma = 0.3;
+s_set = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+results = zeros(size(C_set, 2) * size(s_set, 2), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +27,23 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+for c_iter = 1:size(C_set, 2)
+    for s_iter = 1:size(s_set, 2)
+        
+        model = svmTrain(X, y, C_set(c_iter), ...
+            @(x1, x2) gaussianKernel(x1, x2, s_set(s_iter)));
+        
+        predictions = svmPredict(model, Xval);
+        
+        results((c_iter - 1) * size(C_set, 2) + s_iter) = mean(double(predictions ~= yval));
+    end;
+end;
 
+[rlt, idx] = min(results);
 
+C = C_set(idivide(idx, size(C_set, 2)));
 
-
-
+sigma = s_set(mod(idx, size(C_set, 2)));
 
 % =========================================================================
 
