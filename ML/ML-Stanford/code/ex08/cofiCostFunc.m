f6e434f8d24de647b6e307dab003ccb822d25934
H.-M. Fred Chen <h.m.chen@ieee.org>
2019-05-16 21:11:37 -0700

    feat: add code for cost function w/ regularization of collaborative filtering

--- ML/ML-Stanford/code/ex08/cofiCostFunc.m
+++ ML/ML-Stanford/code/ex08/cofiCostFunc.m
@@ -41,7 +41,8 @@ Theta_grad = zeros(size(Theta));
 %
 
 
-J = sum(sum(((X * Theta') .* R - Y) .^2 / 2));
+J = sum(sum(((X * Theta') .* R - Y) .^2 / 2)) + ...
+    sum(sum(Theta.^2)) * lambda/2 + sum(sum(X.^2)) * lambda/2;
 
 X_grad = ((X * Theta') .* R - Y) * Theta;
 
