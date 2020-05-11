c7a94e3dcea14526897a1ade7e9e3fa67dbf152b
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-11 12:47:48 -0700

    feat; add code to display figure w/ multiple axis

--- Stats/ProbStatsPython/src/topic03-matplotlib.py
+++ Stats/ProbStatsPython/src/topic03-matplotlib.py
@@ -143,6 +143,28 @@ def bar_plot():
     return None
 
 
+"""
+Plotting a figure w/ multiple y-axis
+"""
+
+def maxes_plot():
+    # Make-up data
+    calories = [380.70, 420.98, 454.91, 406.45, 446.16, 498.08, 504.54, 459.05, 459.55, 484.79]
+    countries = ['India', 'Japan', 'Korea', 'China', 'Thai', 'Italy', 'France', 'Greece', 'Mexico', 'US']
+    obesity_rates = [3.9, 4.3, 4.7, 6.2, 10, 19.9, 21.6, 24.9, 28.9, 36.2]
+
+    fig, ax1 = plt.subplots(figsize=(8, 5))
+    fig.suptitle('Multiple axis w/ bar chart')
+    ax1.bar(countries, obesity_rates, color='C8')
+    ax1.set_ylabel('Obesity rate(%)', color='C8')
+    ax1.tick_params(axis='y', labelcolor='C8')
+
+    ax2 = ax1.twinx()
+    ax2.plot(countries, calories, color='C0')
+    ax2.set_ylabel('Calories', color='C0')
+    ax2.tick_params(axis='y', labelcolor='C0')
+    plt.show()
+
 
 def main():
 
@@ -162,7 +184,10 @@ def main():
     # axes_subplots()
 
     # bar plot
-    bar_plot()
+    # bar_plot()
+
+    # multiple axes
+    maxes_plot()
 
     # input("\nPress Enter to continue ...")
 
