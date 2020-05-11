b01900e12cf1d0323416fccb3274fcffcdb1a20f
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-11 12:14:19 -0700

    feat: add code for subplots sharing same axes

--- Stats/ProbStatsPython/src/topic03-matplotlib.py
+++ Stats/ProbStatsPython/src/topic03-matplotlib.py
@@ -100,6 +100,31 @@ def subplots():
     return None
 
 
+def axes_subplots():
+    """ display subplots sharing same axes
+    """
+    # gerenate data
+    x = np.arange(0, 6 * np.pi+0.2, 0.2)
+    y_1 = np.cos(x)
+    y_2 = np.sin(2*x)
+    y_3 = y_1 + y_2
+
+    # display multiple
+    fig, axs = plt.subplots(3, 1, sharex=True)
+    fig.suptitle('Subplots w/ shared axes')
+    axs[0].plot(x, y_1)
+    axs[1].plot(x, y_2)
+    axs[2].plot(x, y_3)
+    axs[0].set_ylabel('$y$')
+    axs[1].set_ylabel('$y$')
+    axs[2].set_ylabel('$y$')
+
+    plt.show()
+
+    return None
+
+
+
 
 def main():
 
@@ -113,9 +138,12 @@ def main():
     # tgfs_plot()
 
     # display multiple figures
-    subplots()
+    # subplots()
 
-    input("\nPress Enter to continue ...")
+    # subplots sharing axes
+    axes_subplots()
+
+    # input("\nPress Enter to continue ...")
 
 
     return None
