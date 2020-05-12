e153a26868471adbd2b7cbca2ff51311e0fe7e87
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-11 20:19:41 -0700

    feat: init Topic 3 Lecture Python code

---
+++ Stats/ProbStatsPython/src/Topic03_Lecture.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -* coding: utf-8 -*-
+
+def main():
+
+
+    return None
+
+if __nmae__ == "__main__":
+
+    print("\nEntering Topic 3 Lecture Counting ...")
+
+    main()
+
+    print("\nEnd Topic 3 Lecture Counting ...")
