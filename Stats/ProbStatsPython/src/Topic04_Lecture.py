9e7acb42371cf08ab3d093ef3e1b9939eb3e2766
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-17 16:15:01 -0700

    feat: add code for permutations built-in and recursive

--- Stats/ProbStatsPython/src/Topic04_Lecture.py
+++ Stats/ProbStatsPython/src/Topic04_Lecture.py
@@ -6,8 +6,43 @@ import math
 import itertools
 
 
+def permute(A, debug=False):
+    """Customized permutation by recursion
+
+    Arguments:
+        A {list} -- a given list
+    """
+    if len(A) == 1:
+        if debug: print(A, [tuple(A)])
+        return [tuple(A)]
+
+    permutations = []
+
+    for x in A:
+        for y in permute(A-{x}, debug):
+            permutations.append((x,) + y)
+            if debug: print(x, y, permutations)
+            
+    return permutations
+
 
 def main():
+
+    print("\n... Permutations ...")
+    print("\ncustomized permutation\n")
+    A = {'a', 'b', 6}
+    print("  Permutations of {} w/ length= {}:\n  {}".format(A, len(permute(A)), permute(A, False)))
+
+    print("\nitertools permutation\n")
+    A = 'a1c3'
+    perm = set(itertools.permutations(A))
+    print("  Permutations of {} w/ length= {}:\n  {}".format(A, len(perm), perm))
+
+    input("\nPress Enter to continue ...")
+
+
+
+
 
     return None
 
