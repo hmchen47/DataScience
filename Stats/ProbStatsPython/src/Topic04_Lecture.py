910709a1db7334267d1dde66c1196ec769d72418
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-17 16:13:10 -0700

    feat: init Topic 4 Python code template

---
+++ Stats/ProbStatsPython/src/Topic04_Lecture.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import math
+import itertools
+
+
+
+def main():
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 4: Combinatorics Python code ...")
+
+    main()
+
+    print("\nEnd Topic 4: Combinatorics Python code ...\n")
