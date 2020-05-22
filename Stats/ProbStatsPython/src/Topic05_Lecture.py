82e1afd1ce6b2be5f417575d650e65846e1c290e
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-22 11:39:15 -0700

    feat: add code for Topic 5.1 Matplotlib and Numpy

--- Stats/ProbStatsPython/src/Topic05_Lecture.py
+++ Stats/ProbStatsPython/src/Topic05_Lecture.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def main:
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+
+
+
+def main():
+
+    # basic configuration for plots
+    plt.style.use([{
+        "figure.figsize": (12, 9),       # figure size
+        "xtick.labelsize": "large",         # font-size pf the X-ticks
+        "ytick.labelsize": "large",         # font-size Y-ticks
+        "legend.fontsize": "x-large",       # font size of the legend
+        "axes.labelsize": "x-large",        # font size of the label
+        "axes.titlesize": "xx-large",       # font title size of title
+        "axes.spines.top": False,
+        "axes.spines.right": False,
+    }, 'seaborn-poster'])
 
 
     return None
@@ -12,4 +31,4 @@ if __name__ == "__main__":
 
     main()
 
-    print("\End Topic 5 Intro to Probability Python code...")
+    print("\nEnd Topic 5 Intro to Probability Python code...")
