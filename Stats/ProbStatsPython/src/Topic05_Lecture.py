a6243468d88f557825432ee6e7f29181ebc2290d
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-22 11:41:14 -0700

    feat: add code for 5.2 Coin flips in Topic 5 Lecture NB

--- Stats/ProbStatsPython/src/Topic05_Lecture.py
+++ Stats/ProbStatsPython/src/Topic05_Lecture.py
@@ -5,6 +5,68 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def coin_flip_plot(p, n=1000, debug=False):
+    """randomly toss coin for n times, count the number of heads and tails, and visualize them
+
+    Arguments:
+        p {float} -- probability of flipping coin w/ tail
+
+    Keyword Arguments:
+        n {int} -- the number of coin tosses (default: {1000})
+        debug {bool} -- turn on or off the debugging message (default: {False})
+    """
+
+    tosses = np.random.choice(["h", "t"], p=[1-p, p], size=n)
+    if debug: 
+        print("\nlist of tossing result ({}): {}".format(n, ', '.join(tosses)))
+
+    # count hte number of heads and tails
+    heads = list(tosses).count("h")
+    tails = list(tosses).count("t")
+
+    if debug:
+        print("{} heads and {} tails".format(heads, tails))
+
+    # bar plot for the coin flips
+    plt.bar([0, 1], [heads, tails], tick_label=['h', 't'], align='center')
+    plt.ylim([0, 10])
+    plt.show()
+
+    return None
+
+
+def simulate_coin_tosses(p, n_tosses=1000, n_simulations=10, debug=False):
+    """perform n_simulations simulations each simulation emulates n_tosses coin flips and plot the 
+    fractions of heads as the tosses number increases.
+
+    Arguments:
+        p {float} -- probability of flipping head in a toss
+
+    Keyword Arguments:
+        n_tosses {int} -- the number of tosses in a simulation
+        n_simulations {int} -- the number of simulations
+        debug {bool} -- turn on or off the debugging message (default: {False})
+    """
+
+    for _ in range(n_simulations):
+        # create three arrays consisting of: the coin flips, their running sums, and partial estimates
+        tosses = np.random.choice([0, 1], p=[1-p, p], size=n_tosses)
+        partial_sums = np.cumsum(tosses)
+        partial_means = partial_sums / np.arange(1, n_tosses+1)
+
+        # plot the partial estimates
+        plt.plot(np.arange(1, n_tosses+1), partial_means)
+
+    # plt.plot(range(n_tosses), [p]*n_tosses, 'k', linewidth=5.0, leabel='p')   # plot the value p
+    plt.plot(range(n_tosses), [p]* n_tosses, 'k', linewidth=5.0, label = 'p')
+
+    plt.xlabel('Number of coin tosses')
+    plt.ylabel('Fraction of heads')
+    plt.xlim([1, n_tosses])         # plot limits
+    plt.legend()
+    plt.show()
+
+    return None
 
 
 
@@ -22,6 +84,16 @@ def main():
         "axes.spines.right": False,
     }, 'seaborn-poster'])
 
+    # coin flip
+    p, n = 0.5, 10
+    # coin_flip_plot(p, n, True)
+
+    p, n_tosses, n_simulations = 0.5, 1000, 5
+    simulate_coin_tosses(p, n_tosses, n_simulations, False)
+
+
+
+    # input("Press Enter to continue ...")
 
     return None
 
