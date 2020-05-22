8d0423d0369ec16d912e5be7d5e6acc1b4b79937
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-22 09:33:17 -0700

    feat: init template for Topic 5 Lecture Python code

---
+++ Stats/ProbStatsPython/src/Topic05_Lecture.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def main:
+
+
+    return None
+
+if __name__ == "__main__":
+
+    print("\nStart Topic 5 Intro to Probability Python code ...")
+
+    main()
+
+    print("\End Topic 5 Intro to Probability Python code...")
