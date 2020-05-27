18680b677bbf2487f707ca634148c8742a6af0ea
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-05-26 21:39:46 -0700

    feat: add code for df columns, count, head in Pandas tutorial

--- Stats/ProbStatsPython/src/Topic06_Lecture_pandas.py
+++ Stats/ProbStatsPython/src/Topic06_Lecture_pandas.py
@@ -25,6 +25,29 @@ def main():
     print("\ndf_heroes_info: \n{}".format(df_heroes_info.head()))
     print("\ndf_heroes_powers: \n{}".format(df_heroes_powers.head()))
 
+    input("\nPress Enter to continue ...")
+
+    # querying the dataset
+    print("\ndf_heros_info['name']: \n{}".format(df_heroes_info['name']))
+    print("\ntype(df_heroes_info['name']: {}".format(type(df_heroes_info['name'])))
+
+    input("\nPress Enter to continue ...")
+
+
+    # columns() to access all the available possible powers
+    list_powers = [power for power in df_heroes_powers.columns][1:]
+    print("\nList of powers:")
+    for powers in list_powers:
+        print("  ", powers)
+
+    input("\nPress Enter to continue ...")
+
+
+    # counting the number of records in a column
+    print("\ndf_heroes_info['Alignment'].value_counts(): \n{}".format(df_heroes_info['Alignment'].value_counts()))
+
+    input("\nPress Enter to continue ...")
+
 
     return None
 
@@ -35,6 +58,6 @@ if __name__ == "__main__":
 
     main()
 
-    print("\nEns Topic 6 Pandas python code ...\n")
+    print("\nEnd Topic 6 Pandas python code ...\n")
 
 
