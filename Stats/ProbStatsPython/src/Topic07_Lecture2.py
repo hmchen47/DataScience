311c7ad921b2d89e90e5cde36c102511027eefb1
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-04 11:27:41 -0700

    feat: init template for Topic 7 Lecture 2 Python code

---
+++ Stats/ProbStatsPython/src/Topic07_Lecture2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -8-
+
+
+import numpy as np 
+from math import sqrt
+
+
+def main():
+
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 7 Lecture NB 2 Python code ...")
+
+    main()
+
+    print("\nEnd Topic 7 Lecture NB 2 Python code ...\n")
