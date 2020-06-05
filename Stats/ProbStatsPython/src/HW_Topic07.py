b875b2b21096da2a5184a74d5b372bfa9440d1f4
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-05 11:17:20 -0700

    feat: add code for Ex4 average_sample_median in T07 HW

--- Stats/ProbStatsPython/src/HW_Topic07.py
+++ Stats/ProbStatsPython/src/HW_Topic07.py
@@ -18,25 +18,39 @@ def median_cal(p):
         elif cum > 0.5:
             return idx+1
 
-def sample_median(n, pdf):
+def sample_median(n, pmf):
     """return median of n generated random numbers w/ given probability
 
     Args:
         n (int): number of random values
-        pdf (list): a list of probability distribution
+        pmf (list): a list of probability distribution
     """
     # generate the list of random values
-    seq = np.random.choice(range(1, len(pdf)+1), size=n, p=pdf)
+    seq = np.random.choice(range(1, len(pmf)+1), size=n, p=pmf)
     # print("\nseq = {}".format(seq))
 
     return stat.median(seq)
 
-def expected_cal(pdf):
+def expected_cal(pmf):
     exp = 0.0
-    for i, val in enumerate(pdf):
+    for i, val in enumerate(pmf):
         exp += val *(i+1)
 
     return exp
+
+
+def average_sample_median(n, pmf):
+    """return the average M_n of 100 samples of size n sampled from the distribution
+
+    Args:
+        n (int): size n sampled from the distributed pmf
+        pmf (list): probability density 
+    """
+    medians = [0.0] * 1000
+    for i in range(1000):
+        medians[i] = sample_median(n, pmf)
+
+    return stat.mean(medians)
 
 
 if __name__ == "__main__":
@@ -73,6 +87,22 @@ if __name__ == "__main__":
     print(expected_cal([0.25,0.25,0.25,0.25]))
     print(expected_cal([0.3,0.4,0.3]))
     print(expected_cal([0.12, 0.04, 0.12, 0.12, 0.2, 0.16, 0.16, 0.08]))
+
+    input("\nPress Enter to continue ........................")
+
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])))
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(10,[0.3,0.4,0.3])))
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(10,[0.99,0.01])))
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(20,[0.4,0.6])))
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(200,[0.1,0.2,0.3,0.1,0.1,0.2])))
+    print()
+    print("\naverage_sample_median(10,[0.2,0.1,0.15,0.15,0.2,0.2])= {}".format(\
+        average_sample_median(100,[0.12, 0.04, 0.12, 0.12, 0.2, 0.16, 0.16, 0.08])))
 
 
     print("\nEnf Topic 7 HW Python code .....\n")
