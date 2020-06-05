9ceb057674877824942c2836ac8db4f43f672387
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-05 10:37:25 -0700

    feat: add code for sample_median in T07 HW

--- Stats/ProbStatsPython/src/HW_Topic07.py
+++ Stats/ProbStatsPython/src/HW_Topic07.py
@@ -18,6 +18,19 @@ def median_cal(p):
         elif cum > 0.5:
             return idx+1
 
+def sample_median(n, pdf):
+    """return median of n generated random numbers w/ given probability
+
+    Args:
+        n (int): number of random values
+        pdf (list): a list of probability distribution
+    """
+    # generate the list of random values
+    seq = np.random.choice(range(1, len(pdf)+1), size=n, p=pdf)
+    # print("\nseq = {}".format(seq))
+
+    return stat.median(seq)
+
 
 if __name__ == "__main__":
 
@@ -29,6 +42,23 @@ if __name__ == "__main__":
     print("median of {}: {}".format(p1, median_cal(p1)))
     print("median of {}: {}".format(p2, median_cal(p2)))
     print("median of {}: {}".format(p3, median_cal(p3)))
+
+    input("\nPress Enter to continue ........................")
+
+    print(sample_median(10, [0.1, 0.2, 0.1, 0.3, 0.1, 0.2])) 
+    print(sample_median(10, [0.1, 0.2, 0.1, 0.3, 0.1, 0.2]))
+
+    print(sample_median(5, [0.3,0.7]))
+    print(sample_median(5, [0.3,0.7]))
+
+    print(sample_median(10,[0.1,0.2,0.3,0.2,0.2]))
+    print(sample_median(10,[0.1,0.2,0.3,0.2,0.2]))
+
+    print(sample_median(25,[0.2,0.1,0.2,0.3,0.1,0.1]))
+    print(sample_median(25,[0.2,0.1,0.2,0.3,0.1,0.1]))
+
+    print(sample_median(10, [0.12,0.04,0.12,0.12,0.2,0.16,0.16,0.08]))
+    print(sample_median(10, [0.12,0.04,0.12,0.12,0.2,0.16,0.16,0.08]))
 
 
     print("\nEnf Topic 7 HW Python code .....\n")
