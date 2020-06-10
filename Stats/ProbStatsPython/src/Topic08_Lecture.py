3aa830be2834216a709bdba44c8a1764920eff1f
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-10 12:25:18 -0700

    feat: init template for Topic 8 Discrete Distribution Family

---
+++ Stats/ProbStatsPython/src/Topic08_Lecture.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -8-
+
+
+def main():
+
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 8 Discrete Distribution Functions Lecture Python Code ...")
+
+    main()
+
+    print("\nEnd of Topic 8 Discrete Distribution Functions Lecture Python Code ...")
