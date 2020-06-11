5d7f5ae17692c78b6f16a27def16565faa13c831
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-10 19:55:05 -0700

    feat: add code to plot Poisson and comparison w/ Binomial

--- Stats/ProbStatsPython/src/Topic08_Lecture.py
+++ Stats/ProbStatsPython/src/Topic08_Lecture.py
@@ -49,7 +49,7 @@ def plot_binom_pmf(n, p, samples=200, histogram=False):
 
     plt.title('PMF of Bin({:d}, {:.2f})'.format(n, p))
     plt.xlabel('k')
-    plt.ylabel('$B_{20, 0.3}(k)')
+    plt.ylabel('$B_{20, 0.3}(k)$')
     plt.show()
 
     return None
@@ -62,9 +62,72 @@ def gen_plot_binomial():
 
     n, p, smp, hist = 100, 0.4, 200, True
     plot_binom_pmf(n, p, samples=smp, histogram=hist)
+ 
+    return None
 
 
-    input("\nPress Enter to continue ....................................")
+def f_poisson(n, lamb, samples=100, histogram=False):
+    """plot Poisson PMF
+
+    Args:
+        n (int): size of Poisson distribution
+        lambda (float): parameter of Poisson distribution
+        samples (int, optional): sample size. Defaults to 100.
+        histogram (bool, optional): plot histogram. Defaults to False.
+    """
+    k = np.arange(0, n+1)
+    prob_poisson = poisson.pmf(k, lamb)
+    plt.plot(k, prob_poisson, '-o')
+
+    if histogram:
+        height, y = np.histogram(poisson.rs(size=samples, mu=lamb),\
+            range=(0, n), bins=n+1, normed=True)
+        plt.bar(k, height, color='r')
+
+    plt.title('PMF of Poisson({})'.format(lamb))
+    plt.xlabel('Number of Events')
+    plt.ylabel('Probability of Number of Events')
+    plt.show()
+
+    return None
+
+
+def poisson_approx(n, p):
+    """Poisson approximation of the Binomial distribution
+
+    Args:
+        n (int): size of Binomial distribution n >> 1
+        p (float): probability of Binomial distribution, p << 1
+    """
+    k = np.arange(0, n+1)
+    x = np.linspace(0, n+1, 1000)
+    lam = n*p
+    stddev = lam**0.5
+    pmf_poisson = poisson.pmf(k, lam)
+    pmf_binom = binom.pmf(k, n, p)
+
+    plt.plot(k, pmf_poisson, 'r', label='Poisson({:0.2f})'.format(lam))
+    plt.plot(k, pmf_binom, 'b-', label='Bin({:d}, {:0.2f})'.format(n, p))
+    plt.title("Poisson Approximation of Binomial")
+    plt.xlabel('n')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+    print("|| pmf_poisson - pmf_binom ||\u2081 = {}".format(sum(abs(pmf_poisson - pmf_binom))))
+
+    return None
+
+def gen_plot_poisson():
+
+    # plot Poisson PMF
+    # n = (0, 50), samples = (1, 1000) lambda = (0.0, 30.0)
+    n, samples, lamb = 40, 5000, 20.0
+    f_poisson(n, lamb, samples=samples)
+
+    # Poisson approximation of the Binomial Distribution
+    # n = (2, 1000), p = (0.0, 0.2, 0.001)
+    n, p = 30, 0.15
+    poisson_approx(n, p)
 
     return None
 
@@ -87,7 +150,10 @@ def main():
     # gen_plot_bernoulli()
 
     # Binomial distribution
-    gen_plot_binomial()
+    # gen_plot_binomial()
+
+    # Poisson distribution
+    gen_plot_poisson()
 
 
     return None
