8d6c525d08cbfaf0ee2e44a44f8ea8e4f4ea8064
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-16 18:55:30 -0700

    feat: init template for Topic 9: Continuous Distribution Families

---
+++ Stats/ProbStatsPython/src/Topic09_Lecture.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pandas
+import scipy.stats as stat
+import matplotlib.pyplot as plt 
+
+from matplotlib import axes
+from math import pi, e
+
+def main():
+
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 09 Lecture Python code ....")
+
+    main()
+
+    print("\nEnd Topic 09 Lecture Python code ....\n")
+
+
