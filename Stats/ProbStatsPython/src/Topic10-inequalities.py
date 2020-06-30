bac0df336362745be25699dd735ee7bb3f7dd0f9
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-29 20:24:02 -0700

    feat: init Topic 10 Inequalities and Central Limit Theorem

---
+++ Stats/ProbStatsPython/src/Topic10-inequalities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd 
+
+from math import sqrt, exp, factorial
+
+
+    
+
+
+def main():
+
+    plt.style.use([{
+        figure.fixsize: (12, 9),
+        "xtrick.labelsize": "large",
+        "ytrick.labelsize": "large",
+        "legend.fontsize": "x-large",
+        "axes.labelsize": "x-large",
+        "axes.titlesize": "xx-large",
+        "axes.spines.top": False,
+        "axes.spines.right": False,
+    }, 'seaborn-poster'])
+
+
+    Input("Press Enter to continue ......")
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 10 Lecture NB .......")
+
+    main()
+
+    print("\nEnd Topic 10 Lecture NB .......\n")
