d7e39c822740f23e909f54ae2dd53ec6792fa4dc
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-06-30 15:56:24 -0700

    feat: add code to plot uniform distribution w/ multiple r.v.s

--- Stats/ProbStatsPython/src/Topic10-inequalities.py
+++ Stats/ProbStatsPython/src/Topic10-inequalities.py
@@ -37,6 +37,76 @@ def plot_markov_chebyshev(mu, sig):
 
     return None
     
+def generate_uniform_counts(a, b, k, n):
+    """generate samples of empirical mean of k uniform 
+    distribution distribution on [a, b]
+
+    Args:
+        a (int): lower part of unifrom distribution
+        b (int): upper part of uniform distribution
+        k (int): number of random variables w/ uniform distribution
+        n (int): number of samples
+    """
+    # generate a k by n matrix of uniform random numbers
+    X = np.random.uniform(a, b, [k, n])
+    S = np.sum(X, axis=0) / k
+    return S
+
+def uniform_plot_hist(s, k, h):
+    """plot uniform distribution w/ histogram
+
+    Args:
+        s (int): width of uniform distribution
+        k (int): number of random variables w/ uniform distribution
+        h (int): number of samples
+    """
+    a, b = s
+    if h > 0:
+        n = h
+        counts = generate_uniform_counts(a, b, k, n)
+        plt.hist(counts, bins=30, density=True, label=('Histogram of mean values'))
+        plt.xlim([0, 10])
+        plt.plot([(a+b)/2, (a+b)/2], [0, 1], 'g--', linewidth=2.0, \
+            label='Mean of unifrom distribution')
+        
+    return None
+
+
+def uniform_mean_pdf(s, n, h, debug=False):
+    """plot the pdf of 1/n(\sum_{i=1}^n X_i), X_i \sim U_{a, b}
+
+    Args:
+        s (int): width of uniform distribution
+        n (int): number of random variables w/ uniform distribution
+        h (int): number of samples
+    """
+    a, b = s
+    d = 10.0/1000;
+    x = np.linspace(0.01, 10, 1000)
+    plt.close()
+
+    if a < b:
+        # ideal uniform distribution w/ given values [a, b]
+        y = (1.0 * (x >= a)) * (1.0 * (x <= b)) / (b-a)
+        z = y
+        for j in range(2, n+1):
+            t = [item/(j-1) for item in z for i in range(j-1)]
+            z = [0, ] + np.convolve(y, t).tolist()
+            z = [i*d for i in z]
+            if debug:
+                print(len(z), "\n",  z)
+                print(np.reshape(z, (1000, j)))
+            z = np.sum(np.reshape(z, (1000, j)), axis=1)
+        plt.plot(x, z, label='Distribution of Mean')
+        uniform_plot_hist(s, n, h)
+        plt.title('PDF and histogram of $\overline{X}_n$ with n=%d, s=%d'%(n,h), fontsize = 20)
+        plt.xlabel('$\overline{X}_n$')
+        plt.ylabel('$f_{\overline{X}_n}(x)$')
+        plt.ylim([0, 1.1])
+        plt.legend()
+        plt.show()
+
+    return None
 
 
 def main():
@@ -54,18 +124,28 @@ def main():
 
     # Inequalities
     # mu = (0.5, 20)  sig = (0.5, 20)
-    mu, sig = 5, 0.5
-    plot_markov_chebyshev(mu, sig)
-    mu, sig = 5, 1
-    plot_markov_chebyshev(mu, sig)
-    mu, sig = 5, 5
-    plot_markov_chebyshev(mu, sig)
-    mu, sig = 5, 10
-    plot_markov_chebyshev(mu, sig)
-    mu, sig = 5, 20
-    plot_markov_chebyshev(mu, sig)
+    # mu, sig = 5, 0.5
+    # plot_markov_chebyshev(mu, sig)
+    # mu, sig = 5, 1
+    # plot_markov_chebyshev(mu, sig)
+    # mu, sig = 5, 5
+    # plot_markov_chebyshev(mu, sig)
+    # mu, sig = 5, 10
+    # plot_markov_chebyshev(mu, sig)
+    # mu, sig = 5, 20
+    # plot_markov_chebyshev(mu, sig)
 
-    Input("Press Enter to continue ......")
+
+    # Uniform distribution: [a, b]
+    # k uniform distributions w/ n repetition
+    # width = [a, b], min(a, b) = 0.02, max(a, b) = 9.98
+    # n = (1, 20),  h = (1, 10000)
+    s, n, h = (2, 8), 5, 10000
+    # uniform_mean_pdf(s, n, h, True)
+    uniform_mean_pdf(s, n, h)
+
+
+    # input("\nPress Enter to continue ......")
 
     return None
 
