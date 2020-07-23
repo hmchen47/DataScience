6add0e512f8408f1f31e028afa7a89d8a37764cc
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-22 19:57:53 -0700

    syle: revise for print out in Topic 12.1 Review Linear Algebra

--- Stats/ProbStatsPython/src/Topic12.1-Lectures.py
+++ Stats/ProbStatsPython/src/Topic12.1-Lectures.py
@@ -11,23 +11,23 @@ def main():
 
     # Lists vs Numpy Arrays
     x_vec = np.array([1, 2, 3])
-    print("\nCreate vector x_vec w/ 'np.array([1, 2, 3]'): {}".format(x_vec))
+    print("\n  Create vector x_vec w/ 'np.array([1, 2, 3]'): {}".format(x_vec))
 
     lst_c = [1, 2]
-    print("\nthe list c_lst= {} w/ length= {}".format(lst_c, len(lst_c)))
+    print("\n  the list c_lst= {} w/ length= {}".format(lst_c, len(lst_c)))
     vec_c = np.array(lst_c)
-    print("convert list to vector vec_c w/ 'np.array(lst_c)' and shape (vec_c.shape): {}".format(vec_c, vec_c.shape))
+    print("  convert list to vector vec_c w/ 'np.array(lst_c)' and shape (vec_c.shape): {}".format(vec_c, vec_c.shape))
 
     z = [5, 6]
     ary_z = np.array(z)
-    print("\nthe lst z = {} w/ type {}".format(z, type(z)))
-    print("array ary_z = {} w/ type {}".format(ary_z, type(ary_z)))
+    print("\n  the lst z = {} w/ type {}".format(z, type(z)))
+    print("  array ary_z = {} w/ type {}".format(ary_z, type(ary_z)))
 
     input("\nPress Enter to continue ......")
 
     # array as vector
     v1 = np.array([[1, 2], [3, 4]])
-    print("\create a 2D matrix w/ 'np.array([[1, 2], [3, 4]])': {}".format(v1))
+    print("\ncreate a 2D matrix w/ 'np.array([[1, 2], [3, 4]])': \n{}".format(v1))
 
 
 
