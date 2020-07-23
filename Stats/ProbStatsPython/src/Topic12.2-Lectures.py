72196f82adf7cab2a04572cdf9572855b21d623c
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-23 12:46:29 -0700

    feat: demo code for split matrix into columns in Topic 12.2

--- Stats/ProbStatsPython/src/Topic12.2-Lectures.py
+++ Stats/ProbStatsPython/src/Topic12.2-Lectures.py
@@ -21,6 +21,14 @@ if __name__ == "__main__":
     print("\ncreate B as a 2x3 matrix w/ A.reshape(2, 3): \n{}  w/ shape= {}".format(B, B.shape))
     print("\ntransposing B matrix w/ B.T: \n{} w/ shape= {}".format(B.T, B.T.shape))
 
+    # matrix as a collection of vectors
+    print("\n\nDemo the relationship btw matrix and vectors:")
+    A = np.array(range(6)).reshape(2, 3)
+    print("\ncreate A w/ np.array(range(6)).reshape(2, 3)= \n{}".format(A))
+    print("\nSplitting A into columns w/ np.split(A, 3, axis=1)")
+    columns = np.split(A, 3, axis=1)
+    for i in range(len(columns)):
+        print("column {}\n{}".format(i, columns[i]))
 
 
 
