f74a18a1cfc0c0b68c1fed2b6baf968ad307efdf
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-23 12:15:29 -0700

    feat: init template for Topic 12.2 Matrix Notation & Operations

---
+++ Stats/ProbStatsPython/src/Topic12.2-Lectures.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 12.2 Lecture Notes Python code ......")
+
+
+    print("\nEnd Topic 12.2 Lecture Notes Python code ......\n")
+
+
