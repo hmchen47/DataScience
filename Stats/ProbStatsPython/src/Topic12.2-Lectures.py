8b37b40f969dc9741801f76d7f658ed1135c111c
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-23 12:37:47 -0700

    feat: demo reshape, shape, and transpose of matrix

--- Stats/ProbStatsPython/src/Topic12.2-Lectures.py
+++ Stats/ProbStatsPython/src/Topic12.2-Lectures.py
@@ -13,6 +13,17 @@ if __name__ == "__main__":
 
     print("\nStarting Topic 12.2 Lecture Notes Python code ......")
 
+    # transposing matrix
+    print("\n\ncreate a 2x3 matrix by reshape:")
+    A = np.array(range(6))
+    print("\ninitial A as a 1x6 vector w/ np.array(range(6)): {}  w/ shape= {}".format(A, A.shape))
+    B = A.reshape(2, 3)
+    print("\ncreate B as a 2x3 matrix w/ A.reshape(2, 3): \n{}  w/ shape= {}".format(B, B.shape))
+    print("\ntransposing B matrix w/ B.T: \n{} w/ shape= {}".format(B.T, B.T.shape))
+
+
+
+
 
     print("\nEnd Topic 12.2 Lecture Notes Python code ......\n")
 
