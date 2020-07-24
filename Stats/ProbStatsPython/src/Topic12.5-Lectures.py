281ca5093c6eedd78aa90e724c4cf8c6f00d5c5d
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-24 13:36:53 -0700

    feat: init template for Topic 12.5 Polynomial Regression

---
+++ Stats/ProbStatsPython/src/Topic12.5-Lectures.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 12.5 Lecture Notes Python code ......")
+
+
+
+
+
+    print("\nEnd of Topic 12.5 Lecture Notes Python code ......\n")
+
+
