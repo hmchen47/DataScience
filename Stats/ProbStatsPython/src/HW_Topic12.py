747017e08588fb7704512c0f80c184e0785668c2
H.-M. Fred Chen <h.m.chen@ieee.org>
2020-07-26 10:27:39 -0700

    docs: init template for Topic 12 HW Python code

---
+++ Stats/ProbStatsPython/src/HW_Topic12.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np 
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+def main(debug=False):
+    """main function to execute all
+
+    Args:
+        debug (bool, optional): turn on/off debug message. Defaults to False.
+    """
+
+
+    return None
+
+
+if __name__ == "__main__":
+
+    print("\nStarting Topic 12 HW Python code ......")
+
+    main(debug=True)
+
+    print("\nEnd of Topic 12 HW Python code ......\n")
+
+
